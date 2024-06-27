4e268d69f4a36bbf6ba86d494520585644be9e8b
CLAUDIO LUCAS <claudio.lucas31@gmail.com>
2024-06-26 22:32:45 -0300

    Aula 02 -

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,13 +2,14 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
+
 from fast_zero.app import app
 
 
 def teste_read_root_deve_retonar_ok_e_ola_mundo():
     client = TestClient(app)  # Arrange (organização)
 
-    response = client.get('/')  # Act (ação)
+    response = client.get('/', status_code=HTTPStatus.OK)  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Olá mundo'}
